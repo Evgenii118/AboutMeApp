58e528a0eb2e31581a4fb4b37ed9acf913d39e20
Ewgeny Izyurov <help@izyapp.ru>
2024-10-10 17:51:01 +0500

    Initial commit.

---
+++ AboutMeApp/UserViewController.swift
@@ -0,0 +1,22 @@
+//
+//  UserViewController.swift
+//  AboutMeApp
+//
+//  Created by Ewgeniy Izyurov on 10.10.2024.
+//
+
+import UIKit
+
+final class UserViewController: UIViewController {
+
+    @IBOutlet weak var greetingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+}
